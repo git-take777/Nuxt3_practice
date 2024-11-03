7745fcf2b985670ded0e8a415c231fda0dd13db2
git-take777 <git.coding.life@gmail.com>
2024-11-03 18:39:36 +0900

    Nuxt3 install 型を作成

---
+++ app.vue
@@ -0,0 +1,35 @@
+<script setup>
+const widthInit = Math.round(Math.random() * 10);
+const heightInit = Math.round(Math.random() * 10);
+const width = ref(widthInit);
+const height = ref(heightInit);
+
+
+const reactive = reactive({
+  width: widthInit,
+  height: heightInit
+})
+
+const area2 = computed(() => {
+  return reactive.width * reactive.height;
+})
+// ref属性を使った実装
+const area = () => {
+  return width.value * height.value;
+};
+
+setInterval(() => {
+  width.value = Math.round(Math.random() * 10);
+  height.value = Math.round(Math.random() * 10);
+}, 1000)
+</script>
+<template>
+  <p>縦 {{ height }} 横 {{ width }} の面積は {{ area() }}です。</p>
+  <p>
+    縦 {{ reactive.height }} 横 {{ reactive.width }} の面積は {{ area() }}です。
+  </p>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
---
+++ server/components/method.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ msg }}</p>
+  <button @click="onButtonClick('Hello', $event)">こんにちは</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const msg = ref("まだ");
+const onButtonClick = (label: string, event: Event): void => {
+const target = event.target as HTMLButtonElement;
+const text = target.innerText;
+console.log(label, text);
+}
+msg.value = `${label} と ${text}`
+</script>
+
+<style scoped></style>
